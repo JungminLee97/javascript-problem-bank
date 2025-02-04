40703a47aa9d17f1446d52430b61e26636b09a83
JungminLee97 <jungmin7618@naver.com>
2025-02-04 09:47:11 +0900

    화요일

--- part1/07.반복문/(lv.1)skip-and-stop.js
+++ part1/07.반복문/(lv.1)skip-and-stop.js
@@ -17,7 +17,19 @@
  *
  * @returns {number}
  */
-function skipAndStop() {}
+function skipAndStop() {
+  let num = 0;
+  for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      continue;
+    }
+    num = num + i;
+    if (i == 7) {
+      break;
+    }
+  }
+  return num;
+}
 
 // export를 수정하지 마세요.
 export { skipAndStop };
